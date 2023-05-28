69fa51e792a951616685d5b8796bfc44bdd61500
kxxdree <truebackward@gmail.com>
2023-05-28 15:14:44 +0300

    feat added about page

--- src/components/MainPage/MainPage.vue
+++ src/components/MainPage/MainPage.vue
@@ -34,6 +34,19 @@
             </div>
             <img src="../../assets/images/mainpage-pic.svg" alt="Лого" class="mainpage_wrapper_logo">
         </div>
+        <div class="about">
+            <h2 class="about_title">О сервисе</h2>
+            <p class="about_subtitle"><strong>Солянка Chat</strong> - творческая версия АИ, с помощью которой каждый человек
+                может
+                персонализировать Chat GPT. Помимо <strong>выбора существующей роли</strong> со специальном промтом из
+                списка вы можете
+                создать <strong>собственные правила</strong> адаптировав чат под себя. Все ограничивается только вашими
+                <strong>желаниями</strong>
+            </p>
+        </div>
+        <footer class="footer">
+            <p class="footer_text">©️ Team Солянка 2023</p>
+        </footer>
     </div>
 </template>
 <script>
@@ -42,6 +55,11 @@ export default {
 }
 </script>
 <style lang="scss">
+.mainpage {
+    display: flex;
+    flex-direction: column;
+}
+
 .mainpage_header {
     display: flex;
     align-items: center;
@@ -91,6 +109,7 @@ export default {
     width: 66rem;
     display: flex;
     flex-direction: column;
+    height: 100vh;
 
 
     &_title {
@@ -127,7 +146,37 @@ export default {
 
 .mainpage_wrapper_logo {
     position: absolute;
-    bottom: 0;
     right: 0;
+    bottom: 0;
+}
+
+.about {
+    display: flex;
+    flex-direction: column;
+    margin: auto;
+    text-align: center;
+    width: 66.25rem;
+    margin-top: -15rem;
+
+    &_title {
+        font-size: 3rem;
+        color: #3C1900;
+    }
+
+    &_subtitle {
+        font-size: 3rem;
+        color: #3C1900;
+    }
+}
+
+.footer {
+    position: relative;
+    bottom: 0;
+    text-align: center;
+    border-top: 1px solid #3C1900;
+
+    &_text {
+        font-size: 2rem;
+    }
 }
 </style>
